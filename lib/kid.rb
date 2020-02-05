1cafa8238add5f898ee7b376d95d02973485c4be
Chloe Foronda <caforonda103@yahoo.com>
2020-02-05 05:30:23 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,14 +3,15 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
-  
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  extend MetaDancing
   
   attr_accessor :name
   
   def initialize(name)
     @name = name
   end
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
 end
